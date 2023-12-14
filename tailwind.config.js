b3607d7c23784beddcdf8430e1a02e2f5e7b2dd9
pajarmuharam14 <pajarmuharam321@gmail.com>
2023-12-14 16:54:48 +0700

    add logo and hamburger menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        softorange: "hsl(35, 77%, 62%)",
+        softred: "hsl(5, 85%, 63%)",
+        offwhite: "hsl(36, 100%, 99%)",
+        grayishblue: "hsl(233, 8%, 79%)",
+        darkgrayishblue: "hsl(236, 13%, 42%)",
+        verydarkblue: "hsl(240, 100%, 5%)",
+      },
+    },
   },
   plugins: [],
 };
